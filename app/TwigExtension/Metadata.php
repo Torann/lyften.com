81d0478fcb118542244334ceeb4fc90606afaf74
Daniel Stainback <torann@gmail.com>
2015-11-06 17:32:38 -0500

    Fix opengraph

--- app/TwigExtension/Metadata.php
+++ app/TwigExtension/Metadata.php
@@ -13,6 +13,7 @@ class Metadata extends AbstractExtension
     {
         return [
             'metaDesc' => new \Twig_Function_Method($this, 'getMetaDesc'),
+            'ogImage' => new \Twig_Function_Method($this, 'getOpenGraphImage')
         ];
     }
 
@@ -29,5 +30,30 @@ class Metadata extends AbstractExtension
         $description = $page ? $page->description : $root->description;
 
         return clean_string($description);
+    }
+
+    /**
+     * Process description.
+     *
+     * @param  \Skosh\Content\Content $page
+     * @param  mixed  $parent
+     * @param  string $default
+     * @return string
+     */
+    public function getOpenGraphImage($page, $parent, $default = 'images/share-og.png')
+    {
+        if($page instanceof \Skosh\Content\Post) {
+            return $page->image['full'];
+        }
+
+        if($page instanceof \Skosh\Content\Page) {
+            return $page->image('logo.png', $default);
+        }
+
+        if($page instanceof \Skosh\Content\Doc && $parent instanceof \Skosh\Content\Page) {
+            return $parent->image('logo.png', $default);
+        }
+
+        return $this->getBuilder()->getAsset($default);
     }
 }
